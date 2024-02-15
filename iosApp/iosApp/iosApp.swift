6e48ab05123db71b74b7b1540ff92e32d4eed694
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-02-15 23:34:44 +0100

    Migrate compose to ios integration to UIKit

--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -1,26 +1,19 @@
 import UIKit
-import SwiftUI
 import ComposeApp
 
 @main
-struct iosApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.rootViewController = MainKt.MainViewController()
+            window.makeKeyAndVisible()
         }
+        return true
     }
-}
-
-struct ContentView: View {
-    var body: some View {
-        ComposeView().ignoresSafeArea(.all)
-    }
-}
-
-struct ComposeView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        MainKt.MainViewController()
-    }
-
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
