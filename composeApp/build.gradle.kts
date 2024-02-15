9bd9b37cf0017484eba0eaf38fa72055261869b5
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-02-15 23:34:05 +0100

    Update deps.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -49,7 +49,6 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.material3)
                 implementation(compose.materialIconsExtended)
-                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
                 implementation(libs.materialKolor)
             }
