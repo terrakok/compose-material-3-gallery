83360c82795b2c37a8c098d3c5b8d251c1d686db
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-11-29 14:16:23 +0100

    Add dependency update plugin and upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,23 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     alias(libs.plugins.multiplatform).apply(false)
     alias(libs.plugins.compose).apply(false)
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.composeCompiler).apply(false)
+    id("com.github.ben-manes.versions").version("0.51.0")
+}
+
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
